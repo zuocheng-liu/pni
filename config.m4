52a5091baa40209f2c6e88fe0daaec02ad586382
zuocheng.liu@gmail.com <zuocheng.liu@gmail.com>
2016-09-26 18:20:27 +0800

    remove master

--- config.m4
+++
@@ -1,5 +0,0 @@
-PHP_ARG_ENABLE(pni, whether to enable PNI support,[ --enable-pni Enable PNI support])
-if test "$PHP_PNI" = "yes"; then
-AC_DEFINE(HAVE_PNI, 1, [Whether you have PNI])
-PHP_NEW_EXTENSION(pni, pni.c, $ext_shared)
-fi
--- example/pow.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pow = new PNIFunction(PNIDataType::DOUBLE, 'pow', 'libm.so.6');
-    $a = new PNIDouble(2);
-    $b = new PNIDouble(10);
-    $res = $pow($a, $b);
-    var_dump($res);
-} catch (PNIException $e) {
-}
